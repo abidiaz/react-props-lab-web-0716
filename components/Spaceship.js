920a421629ca2f6fef458b703a1dee7507939c8c
Abigail Diaz <github email address>
2016-09-26 00:03:28 -0400

    Completed Props Lab

---
+++ components/Spaceship.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const colors = [];
+
+class Spaceship extends React.Component {
+  getDefaultProps() {
+    return {
+      hasRockets: false,
+      colors: ['black','red']
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <h3>{this.props.speed}</h3>
+        <h3>{this.props.hasRockets}</h3>
+        <h3>{this.props.colors.join(', ')}</h3>
+      </div>
+      )
+  }
+};
+
+Spaceship.defaultProps = {
+  hasRockets: false,
+  colors: ['black', 'red']
+};
+
+ReactDOM.render(<Spaceship />, document.getElementById('main'));
+
+
+module.exports = Spaceship;
