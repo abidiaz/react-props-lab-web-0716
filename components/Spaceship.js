e593ede70f9bb36d6f2eccd74cc009f425dbd864
Abigail Diaz <github email address>
2016-09-26 00:08:19 -0400

    Updated Spaceship.js file

--- components/Spaceship.js
+++ components/Spaceship.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 // const colors = [];
 
@@ -27,7 +27,7 @@ Spaceship.defaultProps = {
   colors: ['black', 'red']
 };
 
-ReactDOM.render(<Spaceship />, document.getElementById('main'));
+// ReactDOM.render(<Spaceship />, document.getElementById('main'));
 
 
 module.exports = Spaceship;
